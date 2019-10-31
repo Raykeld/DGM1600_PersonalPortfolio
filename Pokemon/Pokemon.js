8cc6150e362c8e58f1559c838962fb1477bdad67
Sam McLain <sammclain5@gmail.com>
2019-10-31 11:00:25 -0600

    added images and fetched code

--- Pokemon/Pokemon.js
+++ Pokemon/Pokemon.js
@@ -1,26 +1,50 @@
-const allPokemon = (async () => {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon/')
-    const myJson = await response.json()
-    console.log(JSON.stringify(myJson))
+async function getAPIData(url) {
+    try {
+        const response = await fetch(url)
+    const data = await response.json()
+    return data
+} catch (error) {
+    console.error(error)
+    }
+}
+
+const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/')
+.then(data  => {
+    for (const pokemon of data.results) {
+        getAPIData(pokemon.url)
+        .then(pokedata => {
+            populationDOM(pokedata)
+        })
+    }
 })
 
-console.log(allPokemon())
+console.log(theData)
 
-// people.forEach(function(person) {
-//     let personDiv = document.createElement('div')
-//     let name = document.createElement('h3')
-//     let gender = document.createElement('p')
-//     let pic = document.createElement('img')
- 
-//     let charNum = getCharNumber(person.url)
- 
-//     name.textContent = person.name
-//     gender.textContent = person.gender
-//     pic.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
- 
-//     personDiv.appendChild(name)
-//     personDiv.appendChild(gender)
-//     personDiv.appendChild(pic)
- 
-//     mainArea.appendChild(personDiv)
-// })
+let mainArea = document.querySelector('main')
+
+function populationDOM(single_pokemon) {
+        let pokeDiv = document.createElement('div')
+        let name = document.createElement('h3')
+        let pic = document.createElement('img')
+
+        pokeDiv.setAttribute('class', 'charDivs')
+        pic.setAttribute('class', 'picDivs')
+
+         let pokeNum = getPokeNumber(single_pokemon.id)
+        
+        name.textContent = single_pokemon.name
+
+        pic.src = `../images/${pokeNum}.png`
+
+        pokeDiv.appendChild(name)
+        pokeDiv.appendChild(pic)
+
+        mainArea.appendChild(pokeDiv)
+}
+
+function getPokeNumber(id) {
+    if(id < 10) return `00${id}`
+    if(id > 9 && id < 100) {
+        return `0${id}`
+    } else return id    
+} 
