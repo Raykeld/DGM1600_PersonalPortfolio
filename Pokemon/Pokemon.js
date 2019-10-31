8306ba74f59c9fa2f364b07384b53a27989b20a6
Sam McLain <sammclain5@gmail.com>
2019-10-31 08:35:42 -0600

    Madde Pokemon.js work and added it to my home page

--- Pokemon/Pokemon.js
+++ Pokemon/Pokemon.js
@@ -1,2 +1,26 @@
+const allPokemon = (async () => {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/')
+    const myJson = await response.json()
+    console.log(JSON.stringify(myJson))
+})
 
+console.log(allPokemon())
 
+// people.forEach(function(person) {
+//     let personDiv = document.createElement('div')
+//     let name = document.createElement('h3')
+//     let gender = document.createElement('p')
+//     let pic = document.createElement('img')
+ 
+//     let charNum = getCharNumber(person.url)
+ 
+//     name.textContent = person.name
+//     gender.textContent = person.gender
+//     pic.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+ 
+//     personDiv.appendChild(name)
+//     personDiv.appendChild(gender)
+//     personDiv.appendChild(pic)
+ 
+//     mainArea.appendChild(personDiv)
+// })
