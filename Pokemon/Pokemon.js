7ac552168cc1fe31f48dbe52ce2ff786b855dad7
Sam McLain <sammclain5@gmail.com>
2019-11-19 13:13:12 -0700

    commit from class

--- Pokemon/Pokemon.js
+++ Pokemon/Pokemon.js
@@ -9,10 +9,23 @@ class Pokemon {
 }
 
 const Mew = new Pokemon(151, 'Mew', 100);
-const Mewtwo = new Pokemon(150, 'Mewtwo', 106);
 
-
-
+document.querySelector('#pokeButton').addEventListener('click', () => {
+    let pokeId = prompt("Provide the Pokemon ID you want to add:")
+    let pokeIdnum = parseInt(pokeId, 10)
+    if(!pokeIdnum < 807) {
+        alert('That pokemon ID doesnt not exist! Please enter a different ID')
+        return
+    } else {
+    }
+getAPIData(`https://pokeapi.co/api/v2/${pokeId}`)
+    .then(result => {
+        //let newPokemon = new Pokemon(results)
+    populateDOM(result)
+    })
+    .catch(error => console.log(error))
+    
+})
 
 async function getAPIData(url) {
     try {
@@ -24,18 +37,18 @@ async function getAPIData(url) {
     }
 }
 
+
+
 const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/')
 .then(data  => {
     for (const pokemon of data.results) {
         getAPIData(pokemon.url).then(pokedata => {
             populateDOM(pokedata)
-            populateDOM(Mew)
-            populateDOM(Mewtwo)
         })
     }
 })
 
-
+let mainHeader = document.querySelector('header')
 let mainArea = document.querySelector('main')
 
 function populateDOM(single_pokemon) {
@@ -70,7 +83,8 @@ function populateDOM(single_pokemon) {
     let pokeNum = getPokeNumber(data.id)
     pokeFront.appendChild(name)
     name.textContent = `${data.name}`
-    pic.src = `../images/${pokeNum + "MS"}.png`
+    //pic.src = `../images/${pokeNum + "MS"}.png`
+    pic.src = `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${pokeNum}.png`
     
    pokeFront.appendChild(pic)
    pokeFront.appendChild(name)
@@ -82,9 +96,8 @@ function populateDOM(single_pokemon) {
        let pokeHP = document.createElement('h5')
        let pokeType = document.createElement('p')
        pokeOrder.textContent = `#${data.id} ${data.name[0].toUpperCase()}${data.name.slice(1)}`
-       pokeHP.textContent = data.stats[0].base_stat
-       pokeType.textContent = `${data.type}`
-       pokeBack.appendChild(pokeOrder)
+    //    pokeHP.textContent = data.stats[0].base_stat
+       pokeType.textContent = `${data.type[0].type.name}`
        pokeBack.appendChild(pokeHP)
        pokeBack.appendChild(pokeType)
        console.log(pokeType)
@@ -97,7 +110,29 @@ function getPokeNumber(id) {
     if(id < 10) return `00${id}`
     if(id > 9 && id < 100) {
         return `0${id}`
-    } else return id    
+    } else return id 
 } 
 
+let grassButton = document.createElement('button')
+grassButton.textContent = "Grass"
+
+let fireButton = document.createElement('button')
+fireButton.textContent = "Fire"
+
+let waterButton = document.createElement('button')
+waterButton.textContent = "Water"
+
+let bugButton = document.createElement('button')
+bugButton.textContent = "Bug"
+
+let flyingButton = document.createElement('button')
+flyingButton.textContent = "Flying"
+
+
+
+mainHeader.appendChild(grassButton)
+mainHeader.appendChild(fireButton)
+mainHeader.appendChild(waterButton)
+mainHeader.appendChild(bugButton)
+mainHeader.appendChild(flyingButton)
 
